76d9a9dd2badf82ac02537cc18f8b75ffb39787a
vs <vsuhachev@yandex.ru>
2017-01-16 20:37:51 +0700

    hide jurors credentials from users

--- app/helpers/jurors_helper.rb
+++ app/helpers/jurors_helper.rb
@@ -1,2 +1,11 @@
 module JurorsHelper
+  def display_user?(obj = nil)
+    permitted_attributes_for(obj || @contest.jurors.build).include?(:user_id)
+  end
+
+  def index_attributes
+    attributes = %i(fio dob location)
+    attributes << :user if display_user?
+    attributes
+  end
 end
