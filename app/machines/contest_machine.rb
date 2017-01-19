c58595ad504f9d3f9bfc858fb841ed0963fe20b0
vs <vsuhachev@yandex.ru>
2017-01-19 13:06:28 +0700

    contest workflow conditions

--- app/machines/contest_machine.rb
+++ app/machines/contest_machine.rb
@@ -2,13 +2,16 @@ class ContestMachine < FiniteMachine::Definition
   initial :draft
 
   events {
-    event :start, :draft => :preparation
+    event :start, :draft => :preparation, unless: -> (target) { target.nominations.empty? || target.criteria.empty? }
     event :un_start, :preparation => :draft
 
-    event :launch, :preparation => :in_progress
+    event :launch, :preparation => :in_progress, unless: -> (target) { target.jurors.empty? || target.photos.empty? }
     event :un_launch, :in_progress => :preparation
 
     event :finish, :in_progress => :finished
+    event :un_finish, :finished => :in_progress
+
     event :archive, :finished => :archived
+    event :un_archive, :archived => :finished
   }
 end
