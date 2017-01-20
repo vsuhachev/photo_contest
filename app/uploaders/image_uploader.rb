39d06dbce1210b4c90c7ff9f123b921dcccab7c1
vs <vsuhachev@yandex.ru>
2017-01-20 12:18:53 +0700

    increase image size

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,7 +11,7 @@ class ImageUploader < Shrine
   plugin :delete_raw
 
   Attacher.validate do
-    validate_max_size 5*1024*1024, message: 'is too large (max is 5 MB)'
+    validate_max_size 35*1024*1024, message: 'is too large (max is 35 MB)'
     validate_mime_type_inclusion ['image/jpeg', 'image/png']
   end
 
