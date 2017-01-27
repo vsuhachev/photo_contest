40e7513366264993dddedd35d3efceada2456a99
vs <vsuhachev@yandex.ru>
2017-01-27 18:42:48 +0700

    refactoring

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -20,22 +20,7 @@ class Rating < ApplicationRecord
   scope :for_photo, -> (photo) { where(photo_id: photo) }
   scope :for_criterion, -> (criterion) { where(criterion_id: criterion) }
   scope :for_juror, -> (juror) { where(juror_id: juror) }
-
-  scope :averages_for_photo, -> (photo) { find_by_sql([<<~SQL, photo]) }
-      SELECT
-        ratings.id,
-        photos.id photo_id, criteria.id criterion_id, avg(value) value
-      FROM
-        photos
-          INNER JOIN criteria on criteria.contest_id = photos.contest_id
-          LEFT OUTER JOIN ratings ON photo_id = photos.id AND criterion_id = criteria.id
-          LEFT OUTER JOIN jurors ON juror_id = jurors.id
-      WHERE
-        (photos.user_id ISNULL OR jurors.user_id ISNULL OR photos.user_id != jurors.user_id)
-        AND photos.id = ?
-      GROUP BY photos.id, criteria.id
-      ORDER BY photos.id, criteria.id
-  SQL
+  scope :averages_by_criterion, -> { group(:criterion_id).average(:value) }
 
   delegate :to_s, to: :criterion
 
